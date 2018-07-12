fa73f26fc0f405bbc4990d23214fdb479bd1836d
Sourav Khan <sourav.khan@accenture.com>
2018-07-12 17:04:33 +0530

    base code added

---
+++ sendmsgtodemoqueue/index.js
@@ -0,0 +1,23 @@
+module.exports = function (context, myTimer) {
+    var azure = require('azure');
+    var timeStamp = new Date().toISOString();
+    
+    if(myTimer.isPastDue)
+    {
+        context.log('JavaScript is running late!');
+    }
+    context.log('JavaScript timer trigger function ran!:', timeStamp);  
+    
+    var message = {
+        body: 'Test message',
+        customProperties: {
+            testproperty: 'TestValue'
+        }};
+    serviceBusService.sendQueueMessage('myqueue', message, function(error){
+        if(!error){
+            // message sent
+        }
+    });
+    
+    context.done();
+};
